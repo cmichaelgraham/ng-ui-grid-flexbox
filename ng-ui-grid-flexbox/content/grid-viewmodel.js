a025ade0f678de09922f9667d217291c3b15ddc9
Graham <ZIK51127@ihs.internal.corp>
2016-01-05 05:27:56 -0700

    displaying grid.  grid grows fine, but won't shrink.  initial starting point has scroll bars

--- ng-ui-grid-flexbox/content/grid-viewmodel.js
+++ ng-ui-grid-flexbox/content/grid-viewmodel.js
@@ -2,8 +2,28 @@
 
     module.controller('gridViewModel', [function () {
 
+        var gridData = [
+            { LastName: 'Graham', FirstName: 'Mike', FavoriteColor: 'green', GitHubID: 'cmichaelgraham' },
+            { LastName: 'Vader', FirstName: 'Darth', FavoriteColor: 'black', GitHubID: 'darthvader' },
+            { LastName: 'Solo', FirstName: 'Han', FavoriteColor: 'white', GitHubID: 'hansolo' },
+            { LastName: 'Hut', FirstName: 'Jabba', FavoriteColor: 'brown', GitHubID: 'jabbathehut' }
+        ];
+
+        var gridOptions = {
+            data: 'vm.gridData',
+            enableGridMenu: true,
+            columnDefs: [
+                { name: 'LastName', field: 'LastName', width: '25%' },
+                { name: 'FirstName', field: 'FirstName', width: '25%' },
+                { name: 'FavoriteColor', field: 'FavoriteColor', width: '25%' },
+                { name: 'GitHubID', field: 'GitHubID', width: '25%' }
+            ]
+        };
+
         var o = {
-            message: 'this is the grid-viewmodel message...'
+            message: 'this is the grid-viewmodel message...',
+            gridData: gridData,
+            gridOptions: gridOptions
         };
 
         return o;
