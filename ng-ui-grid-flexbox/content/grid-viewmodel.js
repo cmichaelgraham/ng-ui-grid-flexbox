3a003580fbf1a0faf77496a2e68ea078ccb1c2b3
Graham <ZIK51127@ihs.internal.corp>
2016-01-05 05:17:41 -0700

    add grid view with header and body flexbox-ed :)

---
+++ ng-ui-grid-flexbox/content/grid-viewmodel.js
@@ -0,0 +1,12 @@
+﻿(function (module) {
+
+    module.controller('gridViewModel', [function () {
+
+        var o = {
+            message: 'this is the grid-viewmodel message...'
+        };
+
+        return o;
+    }]);
+
+}(angular.module('app')));
